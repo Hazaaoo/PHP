94fd5d96205941aa63f0e7129efd01fe17b52267
Hazao <vrostik750@mail.ru>
2026-01-16 12:57:56 +0300

    lab3 num1

--- Lab3/test.php
+++ Lab3/test.php
@@ -1,3 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="test.php" method="get">
+    <input type="text" name="baran">
+    <button type="submit">жми</button>
+</form>
+</body>
+</html>
+
 <?php
-echo "Hello, world!";
+if (isset($_GET['baran'])){
+    // var_dump($_GET['baran']);
+    $zagon = explode(' ', $_GET['baran']);
+    upFunc($zagon);
+    echo implode(' ', $zagon);
+}
+function upFunc(&$zagon){
+for($i = 0; $i < count($zagon); $i++){
+        if($i % 2 > 0){
+            $zagon[$i] = strtoupper($zagon[$i]);
+        }
+    }
+}
 ?>
